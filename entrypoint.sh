fc6da180238be116ff4f4a0b98abfe643c3ef2a8
StaticRocket <35777938+StaticRocket@users.noreply.github.com>
2022-10-27 21:33:05 -0500

    entrypoint: Forgot Alpine uses standard sh
    Signed-off-by: StaticRocket <35777938+StaticRocket@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-[[ -f requirements.txt ]] && pip install --no-cache -r requirements.txt
+[ -f requirements.txt ] && pip install --no-cache -r requirements.txt
 nuitka3 --python-flag=no_site \
     --disable-ccache \
     --python-flag=no_warnings \
